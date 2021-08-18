68e821f235ba4d8669f23ee66f35965a4a909d16
Felipe Duarte <felipesilvapac@gmail.com>
2021-08-18 18:00:58 -0300

    chore: update

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,9 @@
 export default {
   roots: ['<rootDir>/src'],
-  clearMocks: true,
-  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
   testEnvironment: 'node',
   transform: {
-    '.+\\ts$': 'ts-jest'
+    '^.+\\.(ts|tsx)?$': 'ts-jest'
   }
 }
